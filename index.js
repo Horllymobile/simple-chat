bd6731ed652aefcb190686e2e7c856870d8e458e
Horllymobile <horlamidex1@gmail.com>
2020-12-17 15:47:03 +0100

    3 files modified

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 // Import stylesheets
-import './style.css';
+import "./style.css";
+
+import { fromEvent, throttleTime } from "rxjs";
 
 // Write Javascript code!
-const appDiv = document.getElementById('app');
+const appDiv = document.getElementById("app");
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+const button = document.querySelector("button");
+
+fromEvent(button, "click")
+  .subscribe(e => console.log(e));
